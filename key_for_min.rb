a8f29b4b06061849584e338c17b4213667fcabe5
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-04 16:18:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,11 @@ small_key = nil
 small_val = nil
 
 name_hash.each do |key, val|
+<<<<<<< HEAD
   if small_val == nil || val < small_val
+=======
+  if val < small_val && small_val == nil
+>>>>>>> 432c6dc31d8824e9a69e326ce20c42103bf4ec12
     small_key = key
     small_val = val
     end
